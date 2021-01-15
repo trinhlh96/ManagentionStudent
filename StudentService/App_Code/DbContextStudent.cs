43be78d9e09ad74218b001895dfd120083e71c2e
trinhlh96 <trinhlhth1908021@fpt.edu.vn>
2021-01-15 20:08:35 +0700

    hello

---
+++ StudentService/App_Code/DbContextStudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+/// <summary>
+/// Summary description for DbContextStudent
+/// </summary>
+[ServiceContract]
+public class DbContextStudent : DbContext
+{
+    public DbContextStudent() : base("DbContextStudent90")
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public DbSet<Student> Students { get; set; }
+}
